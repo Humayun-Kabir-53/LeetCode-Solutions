b6006677be34bf4a96c229810a7f39511c8f5562
Humayun-Kabir-53 <humayun1729@gmail.com>
2024-10-04 15:13:42 +0600

    Initial commit: Added LeetCode solutions

---
+++ stack/20LC_solution.js
@@ -0,0 +1,23 @@
+var isValid = function(s) {
+    if ( s.length === 0 || s.length%2 !== 0 ) return false;
+    let stack = [];
+    let map = {
+        "(" : ")",
+        "{" : "}",
+        "[" : "]"
+    }
+    // loop through each element of s
+    for ( let c of s ) {
+        if ( s.length === 0 || s.length %2 !== 0 ) return false;
+        if ( c === "(" || c === "{" || c === "[" ) {
+            stack.push(c); // storing opening brackets
+        }
+        else {
+            //let topStack = stack.pop();
+            if( c !== map[stack.pop()] ) return false;
+        }
+    }
+
+    return !stack.length;
+};
+console.log(isValid("[]["));
